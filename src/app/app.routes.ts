f164e1cb649a3a77a16d0a548c38cb6e6faf28fe
Daire Finn <dmanfinn@gmail.com>
2024-07-28 21:52:52 +0100

    Homepage is on root instead of /home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,6 @@ import { HomepageComponent } from './homepage/homepage.component';
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-    },
-    {
-        path: 'home',
         component: HomepageComponent
     },
     {
