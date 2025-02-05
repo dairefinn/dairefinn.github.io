8eb2b9e0475b5947e933de949dbadd3d6565850f
Daire Finn <dmanfinn@gmail.com>
2025-02-05 01:06:52 +0000

    Added expand/collapse to on repeat playlist embed

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 import { PageWrapperComponent } from '../../components/page-wrapper/page-wrapper.component';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   imports: [
     CardComponent,
-    PageWrapperComponent
+    PageWrapperComponent,
+    ButtonComponent
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
 
+  viewingSpotifyPlaylist: boolean;
+
   constructor() {
     document.title = 'Daire Finn / Home';
+    this.viewingSpotifyPlaylist = false;
+  }
+
+  toggleShowSpotifyPlaylist(): void {
+    this.viewingSpotifyPlaylist = !this.viewingSpotifyPlaylist;
   }
 
 }
